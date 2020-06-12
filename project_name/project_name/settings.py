fc6c4fb19be4b20ca9e439cbfb5070395f9f2683
ianjang <ianjang0718@gmail.com>
2020-06-12 19:59:08 +0900

    set-up postgresql

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'project_name.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_db',
+        'USER': 'project_user',
+        'PASSWORD': 'Pass1@3',
+        'HOST': 'project_db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
