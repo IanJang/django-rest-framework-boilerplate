a6351b167e1781ae78d6b192ce1e6f4353e654c2
ianjang <ianjang0718@gmail.com>
2020-06-30 11:54:19 +0900

    separate celery_worker container

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-python manage.py collectstatic --noinput
-python manage.py migrate
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+if [ "${IS_CELERY_WORKER}" == "true" ]
+then
+    celery -A config worker -l info
+else
+    python manage.py collectstatic --noinput
+    python manage.py migrate
+    gunicorn config.wsgi:application --bind 0.0.0.0:8000
+fi
